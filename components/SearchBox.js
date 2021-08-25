3705d3d4ee114f7392f295f770af4f8c5c1a74fb
mata <mata@matas-MacBook-Air.local>
2021-08-25 02:07:44 +0200

    using if loops and some styling

---
+++ components/SearchBox.js
@@ -0,0 +1,63 @@
+import { useState } from "react";
+import cities from "../lib/city.list.json";
+import Link from "next/link";
+
+function SearchBox() {
+  const [query, setQuery] = useState("");
+  const [results, setResults] = useState([]);
+
+  const onChange = (e) => {
+    const { value } = e.target;
+    setQuery(value);
+
+    let matchingCities = [];
+
+    if (value.length > 3) {
+      for (let city of cities) {
+        if (matchingCities.length >= 5) {
+          break;
+        }
+
+        const match = city.name.toLowerCase().startsWith(value.toLowerCase());
+
+        if (match) {
+          const cityData = {
+            ...city,
+            slug: `${city.name.toLowerCase().replace(/ /g, "-")}-${city.id}`,
+          };
+          matchingCities.push(cityData);
+        }
+      }
+    }
+
+    return setResults(matchingCities);
+  };
+
+  return (
+    <div className="search">
+      <input type="text" value={query} onChange={onChange} />
+
+      {query.length > 3 && (
+        <ul>
+          {results.length > 0 ? (
+            results.map((city) => (
+              <li key={city.slug}>
+                <Link href={`/location/${city.slug}`}>
+                  <a>
+                    {city.name}
+                    {city.state ? `, ${city.state}` : ""}
+                    <span>({city.country})</span>
+                  </a>
+                </Link>
+              </li>
+            ))
+          ) : (
+            <li className="search__no-results">No results!</li>
+          )}
+        </ul>
+      )}
+    </div>
+  );
+}
+
+export default SearchBox;
